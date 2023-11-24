7b1cd6f6b6956d944f08b467c15ecb25d1a06a86
zxx2008 <2665954635@qq.com>
2023-11-24 16:16:52 +0800

    使用openmp标准下的多线程打印hello world

---
+++ 线程/hello.c
@@ -0,0 +1,17 @@
+//使用openmp标准线程打印hello world
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main() {
+
+#pragma omp parallel sections
+{
+#pragma omp section
+	printf("[%d]Hello\n", omp_get_thread_num());
+#pragma omp section
+	printf("[%d]World\n", omp_get_thread_num());
+
+}
+	exit(0);
+}
