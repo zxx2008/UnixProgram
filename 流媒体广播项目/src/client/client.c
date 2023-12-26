6d244544cc3a460ba825ae34c82be814b0cfaaca
zxx2008 <2665954635@qq.com>
2023-12-26 18:33:09 +0800

    客户端搭建中

--- 流媒体广播项目/src/client/client.c
+++ 流媒体广播项目/src/client/client.c
@@ -2,19 +2,28 @@
  * @Author: Zu Xixin 2665954635@qq.com
  * @Date: 2023-12-25 22:30:49
  * @LastEditors: Zu Xixin 2665954635@qq.com
- * @LastEditTime: 2023-12-25 22:41:42
+ * @LastEditTime: 2023-12-26 17:45:36
  * @FilePath: /src/client/client.c
  * @Description: 客户端，主进程使用socket接受数据，交给子进程进行解码播放，父子进程通信使用管道进行
  */
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "client.h"
+#include "../include/proto.h"
 /*
 * -M --mgroup 指定多播组
 * -P --port 指定接收器端口
 * -p --player 指定播放器
 * -H --help   显示帮助
 */
+
+//默认客户端设置信息
+struct client_conf_st client_conf = {\
+ .rcvport = DEFAULT_RCVPORT, \
+ .mgroup = DEFAULT_MGROUP, \
+ .player_cmd = DEFAULT_PLAYERCMD \
+};
 
 int main(int argc, char** argv) {
      
---
+++ 流媒体广播项目/src/client/client.h
@@ -0,0 +1,25 @@
+/*
+ * @Author: Zu Xixin 2665954635@qq.com
+ * @Date: 2023-12-26 17:22:44
+ * @LastEditors: Zu Xixin 2665954635@qq.com
+ * @LastEditTime: 2023-12-26 17:46:53
+ * @FilePath: /src/client/client.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef _CLIENT_H_
+#define _CLIENT_H_
+
+#define DEFAULT_PLAYERCMD "/usr/bin/mpg123 > /dev/null"
+
+/**
+ * @description: 客户端配置信息结构体
+ */
+struct client_conf_st {
+    char* rcvport;  //端口
+    char* mgroup;   //多播组地址
+    char* player_cmd;   //播放器命令
+};
+
+
+
+#endif
