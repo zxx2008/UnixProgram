d753f81d5ea97d1f6eedd735b0275db3985eb590
zxx2008 <2665954635@qq.com>
2024-01-06 17:15:05 +0800

    节目单频道线程

--- 流媒体广播项目/src/server/thr_list.c
+++ 流媒体广播项目/src/server/thr_list.c
@@ -15,7 +15,7 @@ static struct mlib_listentry_st *list_entry; //频道列表
 
 static void *thr_list(void *p) {
     int totalsize;
-    struct msg_list_st *entrylistptr;   //节目单描述
+    struct msg_list_st *entrylistptr;   //节目单频道描述
     struct msg_listentry_st *entryptr;  //其他频道描述
 
     int ret;
@@ -23,7 +23,30 @@ static void *thr_list(void *p) {
 
     // 计算需要传输的字节数
     totalsize = sizeof(chnid_t);
-    for(int i = 0; i < )
+    for(int i = 0; i < num_list_entry; ++i) {
+        totalsize += sizeof(struct msg_listentry_st) + strlen(list_entry[i].desc);
+    }
+
+    entrylistptr = malloc(totalsize);
+    if(entrylistptr == NULL) {
+        syslog(LOG_ERR, "malloc(): %s", strerror(errno));
+        exit(1);
+    }
+    entrylistptr->chnid = LISTCHNID;
+    entryptr = entrylistptr->entry; // 不再为entryptr重新分配内存，直接使用entrylistptr中entry数组的内存使用
+    syslog(LOG_DEBUG, "num_list_entry: %d\n", num_list_entry);
+    for(int i = 0; i < num_list_entry; ++i) {
+        size = sizeof(struct msg_listentry_st) + strlen(list_entry[i].desc);
+        entryptr->chnid = list_entry[i].chnid;
+        entryptr->len = htons(size);
+        strcpy(entryptr->desc, list_entry[i].desc); //可以直接使用strcpy，因为entrylistptr->entry内存已经在分配完成
+        entryptr = (void *)(((char *)entryptr) + size); //指针向后移动
+        syslog(LOG_DEBUG, "entry len: %d\n", entryptr->len);
+    }
+
+    while(1) {
+        syslog(LOG_INFO, "thr_list sn")
+    }
 
 }
 
